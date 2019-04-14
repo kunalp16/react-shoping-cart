39994d19ef8caed343381fdb896c52e1805b4531
kunal <kunaljp16@gmail.com>
2019-04-15 00:34:22 +0530

    base set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header>
+        <a className=" float-left nav-link" href="#">Shoping cart</a>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">Login</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Signup</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Wishlist</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Cart</a>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li>
+        </ul>
+
+        {/* <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-interval="1000">
+              <img className="carousel-item" src="//via.placeholder.com/800x400" data-lazy-type="image" data-src="//via.placeholder.com/800x400" />
+            </div>
+              <div className="carousel-item" data-interval="2000">
+              <img className="carousel-item" src="//via.placeholder.com/800x400" data-lazy-type="image" data-src="//via.placeholder.com/800x400" />
+              </div>
+              <div className="carousel-item">
+              <img className="carousel-item" src="//via.placeholder.com/800x400" data-lazy-type="image" data-src="//via.placeholder.com/800x400" />
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div> */}
+        <div className="card-group">
+          <div className="card">
+            <img src="//via.placeholder.com/150x100" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="//via.placeholder.com/150x100" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="//via.placeholder.com/150x100" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+          <div className="card">
+            <img src="//via.placeholder.com/150x100" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </header>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import 'bootstrap/dist/js/bootstrap.min';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
